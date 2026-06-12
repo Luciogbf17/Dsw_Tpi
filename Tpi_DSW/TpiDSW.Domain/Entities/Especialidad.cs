466832135ee8667ac776c3108c976c6a95f54f18
LaureanoGarcia42 <123835658+LaureanoGarcia42@users.noreply.github.com>
2026-06-12 20:59:31 -0300

    Update Especialidad.cs

--- Tpi_DSW/TpiDSW.Domain/Entities/Especialidad.cs
+++ Tpi_DSW/TpiDSW.Domain/Entities/Especialidad.cs
@@ -1,8 +1,37 @@
 ﻿using System;
 
-public class Class1
+public class Especialidad
 {
-	public Class1()
-	{
-	}
+    private string _nombre;
+    private string _descripcion;
+    private bool _activa;
+
+    public Especialidad()
+    {
+    }
+
+    public Especialidad(string nombre, string descripcion, bool activa)
+    {
+        _nombre = nombre;
+        _descripcion = descripcion;
+        _activa = activa;
+    }
+
+    public string Nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+
+    public string Descripcion
+    {
+        get { return _descripcion; }
+        set { _descripcion = value; }
+    }
+
+    public bool Activa
+    {
+        get { return _activa; }
+        set { _activa = value; }
+    }
 }
