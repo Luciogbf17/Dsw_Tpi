8c78cec6f85ebb5429bcd4faf33dbb0f81319b7d
Esteban <estebanadrn@gmail.com>
2026-06-26 16:48:23 -0300

    Subo ValidationException

---
+++ Tpi_DSW/TpiDSW.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TpiDSW.Domain.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(string message) : base(message)
+    {
+    }
+}
